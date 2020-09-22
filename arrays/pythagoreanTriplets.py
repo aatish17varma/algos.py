97ac8c9c2c01da88ffa58fa420c3b4fe3153926b
Aatish Varma <aatish17varma@gmail.com>
2020-09-22 12:50:30 -0400

    pythagorean triplets in O(N^2) time

---
+++ arrays/pythagoreanTriplets.py
@@ -0,0 +1,16 @@
+from math import pow, sqrt
+#O(N^2) solution
+class Solution:
+    def triplets(self, arr):
+        hashMap = {}
+        for i in arr:
+            hashMap[i] = True
+        for i in range(len(arr)):
+            for j in range(i + 1, len(arr)):
+                if sqrt(pow(arr[i],2) + pow(arr[j],2)) in hashMap:
+                    return True
+
+        return False
+one = Solution()
+print(one.triplets([3,5,12,5,13]))
+
